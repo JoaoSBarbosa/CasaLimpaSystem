629df2046d1f1846f48892b7ac4b5de72c4feacc
João Barbosa <contato.jsbarbosa@gmail.com>
2024-10-30 18:59:39 -0300

    :tada: init - Estrutura inicial do projeto

---
+++ src/main/java/com/joaosbarbosa/dev/casaLimpaPlus/CasaLimpaPlusApplication.java
@@ -0,0 +1,13 @@
+package com.joaosbarbosa.dev.casaLimpaPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaLimpaPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaLimpaPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaosbarbosa/dev/casaLimpaPlus/CasaLimpaPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaosbarbosa.dev.casaLimpaPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaLimpaPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
