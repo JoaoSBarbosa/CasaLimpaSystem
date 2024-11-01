075cd257f53e79ba985448d5acb2955ef5184983
João Barbosa <contato.jsbarbosa@gmail.com>
2024-11-01 11:15:22 -0300

    :sparkles: feat - Finalização da model Formulario de Cadastro de serviço; Lógica para envio de enum de icones

--- src/main/java/com/joaosbarbosa/dev/casaLimpaPlus/web/controllers/ServicoController.java
+++ src/main/java/com/joaosbarbosa/dev/casaLimpaPlus/web/controllers/ServicoController.java
@@ -1,8 +1,10 @@
 package com.joaosbarbosa.dev.casaLimpaPlus.web.controllers;
 
 import com.joaosbarbosa.dev.casaLimpaPlus.core.models.Servico;
+import com.joaosbarbosa.dev.casaLimpaPlus.core.models.enums.Icone;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,7 +17,12 @@ public class ServicoController {
     public ModelAndView cadastrar(){
         var mv = new ModelAndView("admin/servico/form");
         mv.addObject("servico", new Servico());
+//        mv.addObject("icones", Icone.values());
         return mv;
     }
 
+    @ModelAttribute("icones")
+    public Icone[] getIcones(){
+        return Icone.values();
+    }
 }
