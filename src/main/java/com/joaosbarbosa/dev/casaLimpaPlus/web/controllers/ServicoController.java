9df5b4201bb6ce9aaad359a1c7f42c1254ab9866
João Barbosa <contato.jsbarbosa@gmail.com>
2024-11-01 21:55:40 -0300

    :construction: - Andamento da validação

--- src/main/java/com/joaosbarbosa/dev/casaLimpaPlus/web/controllers/ServicoController.java
+++ src/main/java/com/joaosbarbosa/dev/casaLimpaPlus/web/controllers/ServicoController.java
@@ -4,6 +4,8 @@ import com.joaosbarbosa.dev.casaLimpaPlus.core.models.Servico;
 import com.joaosbarbosa.dev.casaLimpaPlus.core.models.enums.Icone;
 import com.joaosbarbosa.dev.casaLimpaPlus.core.repository.ServicoRepository;
 import com.joaosbarbosa.dev.casaLimpaPlus.core.service.ServicoService;
+import com.joaosbarbosa.dev.casaLimpaPlus.web.dto.ServicoFormDTO;
+import com.joaosbarbosa.dev.casaLimpaPlus.web.mappers.WebServicoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Controller;
@@ -11,16 +13,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/admin/servicos")
 public class ServicoController {
 
-    @Autowired
-    ServicoService servicoService;
-    @Autowired
-    private ServicoRepository servicoRepository;
-    @Autowired
-    private DefaultErrorAttributes errorAttributes;
+    @Autowired private ServicoService servicoService;
+    @Autowired private ServicoRepository servicoRepository;
+    @Autowired private WebServicoMapper webServicoMapper;
+
 
     @GetMapping
     public ModelAndView listar() {
@@ -32,29 +34,35 @@ public class ServicoController {
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar() {
         var mv = new ModelAndView("admin/servico/form");
-        mv.addObject("servico", new Servico());
+        mv.addObject("formDTO", new ServicoFormDTO());
         return mv;
     }
 
 
     @PostMapping("/cadastrar")
-    public String cadastrarServico(Servico servico) {
-//        servicoService.cadastrarServico(servico);
-        servicoRepository.save(servico);
+    public String cadastrarServico(@Valid ServicoFormDTO formDTO) {
+        var modelServico = webServicoMapper.convertDTOToModel(formDTO);
+        servicoRepository.save(modelServico);
         return "redirect:/admin/servicos";
     }
 
     @GetMapping("/{id}/editar")
     public ModelAndView editarServico(@PathVariable Long id) {
         var mv = new ModelAndView("admin/servico/form");
-        mv.addObject("servico", servicoRepository.findById(id));
+        var modelServico = servicoRepository.findById(id);
+        if(modelServico.isPresent()) {
+            var formDto = webServicoMapper.convertModelToDTO(modelServico.get());
+            mv.addObject("formDTO", formDto);
+        }
+
         return mv;
     }
 
     @PostMapping("/{id}/editar")
-    public String editarServico(@PathVariable Long id, Servico updateService) {
+    public String editarServico( @PathVariable Long id, @Valid ServicoFormDTO updateService) {
 
-        servicoRepository.save(updateService);
+        var modelServico = webServicoMapper.convertDTOToModel(updateService);
+        servicoRepository.save(modelServico);
         return "redirect:/admin/servicos";
     }
 
--- src/main/java/com/joaosbarbosa/dev/casaLimpaPlus/web/mappers/WebServicoMapper.java
+++ src/main/java/com/joaosbarbosa/dev/casaLimpaPlus/web/mappers/WebServicoMapper.java
@@ -11,11 +11,72 @@ import java.util.Optional;
 @Component
 public class WebServicoMapper {
 
-    public Servico convertDTOToModel(ServicoFormDTO dto){
+//    public Servico convertDTOToModel(ServicoFormDTO dto){
+//        if(dto == null) throw new IllegalArgumentException();
+//        var model = new Servico();
+//
+//
+//        if ( dto.getId() != null) model.setId(dto.getId());
+//        if ( dto.getNome() != null) model.setNome(dto.getNome());
+//        if ( dto.getQtdHoras() != null) model.setQtdHoras(dto.getQtdHoras());
+//
+//        if ( dto.getHorasQuarto() != null) model.setHorasQuarto(dto.getHorasQuarto());
+//        if ( dto.getHorasSala() != null) model.setHorasSala(dto.getHorasSala());
+//        if ( dto.getHorasCozinha() !=null) model.setHorasCozinha(dto.getHorasCozinha());
+//        if ( dto.getHorasBanheiro() != null) model.setHorasBanheiro(dto.getHorasBanheiro());
+//        if ( dto.getHorasQuintal() != null) model.setHorasQuintal(dto.getHorasQuintal());
+//        if ( dto.getHorasOutros() != null) model.setHorasOutros(dto.getHorasOutros());
+//
+//        if ( dto.getPosicao() != null) model.setPosicao(dto.getPosicao());
+//
+//        if ( dto.getValorMinimo() != null) model.setValorMinimo(dto.getValorMinimo());
+//        if ( dto.getPorcentagemComissao() != null) model.setPorcentagemComissao( dto.getPorcentagemComissao());
+//        if ( dto.getValorQuarto() != null) model.setValorQuarto( dto.getValorQuarto());
+//        if ( dto.getValorSala() != null) model.setValorSala( dto.getValorSala());
+//        if ( dto.getValorCozinha() != null) model.setValorCozinha( dto.getValorCozinha());
+//        if ( dto.getValorBanheiro() != null) model.setValorBanheiro( dto.getValorBanheiro());
+//        if ( dto.getValorQuintal() != null) model.setValorQuintal( dto.getValorQuintal());
+//        if ( dto.getValorOutros() != null) model.setValorOutros( dto.getValorOutros());
+//        if ( dto.getIcone() != null) model.setIcone( dto.getIcone());
+//
+//        return model;
+//    }
+
+//    public ServicoFormDTO convertModelToDTO(Servico model){
+//        if(model == null) throw new IllegalArgumentException();
+//        var formDto = new ServicoFormDTO();
+//
+//        if ( model.getId() != null) formDto.setId(model.getId());
+//        if ( model.getNome() != null) formDto.setNome(model.getNome());
+//        if ( model.getQtdHoras() != null) formDto.setQtdHoras(model.getQtdHoras());
+//
+//        if ( model.getHorasQuarto() != null) formDto.setHorasQuarto(model.getHorasQuarto());
+//        if ( model.getHorasSala() != null) formDto.setHorasSala(model.getHorasSala());
+//        if ( model.getHorasCozinha() !=null) formDto.setHorasCozinha(model.getHorasCozinha());
+//        if ( model.getHorasBanheiro() != null) formDto.setHorasBanheiro(model.getHorasBanheiro());
+//        if ( model.getHorasQuintal() != null) formDto.setHorasQuintal(model.getHorasQuintal());
+//        if ( model.getHorasOutros() != null) formDto.setHorasOutros(model.getHorasOutros());
+//
+//        if ( model.getPosicao() != null) formDto.setPosicao(model.getPosicao());
+//
+//        if ( model.getValorMinimo() != null) formDto.setValorMinimo(model.getValorMinimo());
+//        if ( model.getPorcentagemComissao() != null) formDto.setPorcentagemComissao( model.getPorcentagemComissao());
+//        if ( model.getValorQuarto() != null) formDto.setValorQuarto( model.getValorQuarto());
+//        if ( model.getValorSala() != null) formDto.setValorSala( model.getValorSala());
+//        if ( model.getValorCozinha() != null) formDto.setValorCozinha( model.getValorCozinha());
+//        if ( model.getValorBanheiro() != null) formDto.setValorBanheiro( model.getValorBanheiro());
+//        if ( model.getValorQuintal() != null) formDto.setValorQuintal( model.getValorQuintal());
+//        if ( model.getValorOutros() != null) formDto.setValorOutros( model.getValorOutros());
+//        if ( model.getIcone() != null) formDto.setIcone( model.getIcone());
+//
+//        return formDto;
+//    }
+
+    public Servico convertDTOToModel(ServicoFormDTO dto) {
         if (dto == null) throw new IllegalArgumentException("DTO não pode ser nulo");
+
         var model = new Servico();
         BeanUtils.copyProperties(dto, model);
-
 
         Optional.ofNullable(dto.getId()).ifPresent(model::setId);
         Optional.ofNullable(dto.getNome()).ifPresent(model::setNome);
@@ -39,7 +100,6 @@ public class WebServicoMapper {
 
         return model;
     }
-
     public ServicoFormDTO convertModelToDTO(Servico model) {
         if (model == null) throw new IllegalArgumentException("Model não pode ser nulo");
 
