7d966df73680d2ebd35a93fbe0e17c4b47017253
João Barbosa <contato.jsbarbosa@gmail.com>
2024-11-06 16:25:00 -0300

    :sparkles: feat: Initial configure authentication login e logout

--- src/main/java/com/joaosbarbosa/dev/casaLimpaPlus/config/SecurityConfig.java
+++ src/main/java/com/joaosbarbosa/dev/casaLimpaPlus/config/SecurityConfig.java
@@ -1,16 +1,24 @@
 package com.joaosbarbosa.dev.casaLimpaPlus.config;
 
+import com.joaosbarbosa.dev.casaLimpaPlus.core.enums.TipoUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userService;
+    @Autowired
+    private PasswordEncoder encoder;
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -20,9 +28,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().permitAll();
 
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasAnyAuthority(TipoUsuario.ADMIN.toString())
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+
+        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout", "GET"))
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/webjar/**");
+    }
 }
